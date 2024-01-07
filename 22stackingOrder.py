8c784aac44e9f7e80a610f00f7566f3c7ffa8bc4
HansHT <johannes.htomas@gmail.com>
2024-01-07 23:17:08 +0000

    Stacking order

---
+++ 22stackingOrder.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = tk.Tk()
+window.geometry('400x400')
+window.title('Template')
+
+# widgets
+label1 = ttk.Label(window, text='Label 1', background='green')
+label2 = ttk.Label(window, text='Label 2', background='red')
+label3 = ttk.Label(window, text='Label 3', background='purple')
+
+
+button1 = ttk.Button(window, text='raise label 1', command= lambda: label1.lift(aboveThis=label2))
+button2 = ttk.Button(window, text='raise label 2', command= lambda: label2.lift())
+button3 = ttk.Button(window, text='raise label 3', command= lambda: label3.lift())
+
+# layout
+label1.place(x=50, y=100, width=200, height=150)
+label2.place(x=150, y=60, width=140, height=100)
+label3.place(x=120, y=40, width=140, height=110)
+
+button1.place(rely=1, relx=0.8, anchor='se')
+button2.place(rely=1, relx=1, anchor='se')
+button3.place(rely=1, relx=0.6, anchor='se')
+
+# run
+window.mainloop()
